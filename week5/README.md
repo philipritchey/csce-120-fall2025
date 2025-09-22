7eba2ec264f420b127fb2048fc55b929526e6cdf
philip ritchey <pcr@tamu.edu>
2025-09-21 23:48:17 -0500

    throwing mechanics code, updated readme for week 5

---
+++ week5/README.md
@@ -0,0 +1,3 @@
+# Week 5: Exceptions, Input/Output Streams
+* [throwing mechanics](throwing_mechanics): examples of throwing stuff
+* [data validation](data_validation): data validation error handling snippets
---
+++ week5/throwing_mechanics/throwing_mechanics.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <stdexcept>
+
+void throw_an_int() {
+    throw 7;
+}
+
+void throw_a_c_string() {
+    throw "ouch!";
+}
+
+void throw_a_std_string() {
+    throw std::string("ouch!");
+}
+
+void throw_a_std_exception() {
+    throw std::invalid_argument("no no no, not in my house.");
+}
+
+int main() {
+    std::cout << "(1) int\n";
+    std::cout << "(2) c string (char const*)\n";
+    std::cout << "(3) std::string\n";
+    std::cout << "(4) std::invalid_argument\n";
+    std::cout << "> ";
+    int number;
+    std::cin >> number;
+    switch (number) {
+    case 1:
+        throw_an_int();
+    case 2:
+        throw_a_c_string();
+    case 3:
+        throw_a_std_string();
+    case 4:
+        throw_a_std_exception();
+    default:
+        throw std::out_of_range("i'm sorry dave, i can't let you do that...");
+    }
+    return 0;
+}
