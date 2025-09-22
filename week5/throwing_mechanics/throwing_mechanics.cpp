1112400c76266b17b2abbc4c2bf360aa104dd473
philip ritchey <pcr@tamu.edu>
2025-09-21 23:52:53 -0500

    added another throw example

--- week5/throwing_mechanics/throwing_mechanics.cpp
+++ week5/throwing_mechanics/throwing_mechanics.cpp
@@ -18,11 +18,24 @@ void throw_a_std_exception() {
     throw std::invalid_argument("no no no, not in my house.");
 }
 
+void f3() {
+    throw std::runtime_error("boo!");
+}
+
+void f2() {
+    f3();
+}
+
+void f1() {
+    f2();
+}
+
 int main() {
     std::cout << "(1) int\n";
     std::cout << "(2) c string (char const*)\n";
     std::cout << "(3) std::string\n";
     std::cout << "(4) std::invalid_argument\n";
+    std::cout << "(5) from the deep\n";
     std::cout << "> ";
     int number;
     std::cin >> number;
@@ -35,6 +48,8 @@ int main() {
         throw_a_std_string();
     case 4:
         throw_a_std_exception();
+    case 5:
+        f1();
     default:
         throw std::out_of_range("i'm sorry dave, i can't let you do that...");
     }
