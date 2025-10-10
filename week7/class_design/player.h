64b04e1794cb359907c07e8e122250d83ffdec9d
philip ritchey <pcr@tamu.edu>
2025-10-10 09:26:39 -0500

    refactor player into header and source files

--- week7/class_design/player.h
+++ week7/class_design/player.h
@@ -12,27 +12,13 @@ class Player {
     std::string position;
 
  public:  // everyting that follows is public
-    void set_name(const std::string& new_name) {
-        name = new_name;
-    }
-
-    void set_jersey_number(unsigned int new_jersey_number) {
-        jersey_number = new_jersey_number;
-    }
-
-    void set_position(const std::string& new_position) {
-        position = new_position;
-    }
-
-    std::string get_name() const { return name; }
-    std::string get_position() const { return position; }
-    unsigned int get_jersey_number() const { return jersey_number; }
-
-    std::string print() const {
-        std::ostringstream oss;
-        oss << std::setw(16) << name << " #" << std::setw(2) << jersey_number << " " << position;
-        return oss.str();
-    }
+    void set_name(const std::string& new_name);
+    void set_jersey_number(unsigned int new_jersey_number);
+    void set_position(const std::string& new_position);
+    std::string get_name() const;
+    std::string get_position() const;
+    unsigned int get_jersey_number() const;
+    std::string print() const;
 };
 
 #endif  // _PLAYER_H_
