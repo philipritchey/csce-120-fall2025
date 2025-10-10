ed8e824857a8a491205042e3a7d3b8a79b56fd51
philip ritchey <pcr@tamu.edu>
2025-10-10 09:31:58 -0500

    refactor team into header and source files

--- week7/class_design/team.h
+++ week7/class_design/team.h
@@ -1,7 +1,6 @@
 #ifndef _TEAM_H_
 #define _TEAM_H_
 
-#include <sstream>
 #include <string>
 #include <vector>
 #include "player.h"
@@ -16,11 +15,7 @@ class Team {
         unsigned num_losses = 0;
         unsigned num_ties = 0;
 
-        std::string print() const {
-            std::ostringstream oss;
-            oss << num_wins << "-" << num_losses << "-" << num_ties;
-            return oss.str();
-        }
+        std::string print() const;
     };
 
     std::string name;
@@ -28,30 +23,12 @@ class Team {
     std::vector<Player> players;
 
  public:
-    std::string get_name() const { return name; }
-    void set_name(const std::string& new_name) { name = new_name; }
-    void add_player(const Player& player) { players.push_back(player); }
-    std::string print_roster() const {
-        std::ostringstream oss;
-        for (const Player& player : players) {
-            oss << player.print() << "\n";
-        }
-        return oss.str();
-    }
-    void update_record(char result) {
-        switch (result) {
-            case WIN:
-                record.num_wins += 1;
-                break;
-            case LOSS:
-                record.num_losses += 1;
-                break;
-            case TIE:
-                record.num_ties += 1;
-                break;
-        }
-    }
-    std::string print_record() const { return record.print(); }
+    void set_name(const std::string& new_name);
+    void add_player(const Player& player);
+    void update_record(char result);
+    std::string get_name() const;
+    std::string print_roster() const;
+    std::string print_record() const;
 };
 
 #endif  // _TEAM_H_
